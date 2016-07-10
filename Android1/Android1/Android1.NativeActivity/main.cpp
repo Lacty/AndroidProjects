064dce58679ac663004da48dace73475d0caf068
Lacty <akira206@gmail.com>
2016-07-10 19:54:55 +0900

    draw point

--- Android1/Android1/Android1.NativeActivity/main.cpp
+++ Android1/Android1/Android1.NativeActivity/main.cpp
@@ -5,8 +5,15 @@
 void android_main(struct android_app* state) {
   AppEngine engine(state);
 
+  float touch_x = 0;
+  float touch_y = 0;
+
   while (1) {
     updateEvents(&engine, state);
+
+    // ‰æ–Ê’†S‚ğ’†“_‚Æ‚µ‚½À•WŒn‚É•ÏŠ·
+    touch_x = (engine.touch_x - engine.width * 0.5f) / (engine.width * 0.5f);
+    touch_y = ((engine.touch_y - engine.height * 0.5f) / (engine.height * 0.5f)) * -1.0f;
 
     if (engine.active) {
       if (engine.display == NULL) return;
@@ -14,6 +21,16 @@ void android_main(struct android_app* state) {
       glClear(GL_COLOR_BUFFER_BIT);
 
       // renderer hear
+
+      GLfloat vtx[] = {
+        touch_x, touch_y
+      };
+
+      glVertexPointer(2, GL_FLOAT, 0, vtx);
+      glEnableClientState(GL_VERTEX_ARRAY);
+      glPointSize(100);
+      glDrawArrays(GL_POINTS, 0, 1);
+      glDisableClientState(GL_VERTEX_ARRAY);
 
       eglSwapBuffers(engine.display, engine.surface);
     }
